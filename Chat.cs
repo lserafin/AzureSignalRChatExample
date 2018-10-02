f153126c1bcb1cce0294428e0367b3df2e5d2e8d
Luzian Serafin <luzianserafin@gmail.com>
2018-10-02 20:55:32 +0200

    First commit

---
+++ Chat.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatExample
+{
+    public class Chat : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRChatExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
